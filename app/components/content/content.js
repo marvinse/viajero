dcaeead2138cb1c2f0f86f23a5ecbae9a8753479
marsolan <marvin.solano@groupeconnect.com>
2018-04-11 12:30:19 -0600

    Initial set up

---
+++ app/components/content/content.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.content = (function () {
+    var initSlickSlider = function(){
+        
+    };
+
+    var bindEventsToUI = function () {
+
+    };
+
+    var init = function () {
+        
+        console.log('APP.content');
+        bindEventsToUI();
+    };
+
+    /**
+     * interfaces to public functions
+     */
+    return {
+        init: init
+    };
+
+}());
