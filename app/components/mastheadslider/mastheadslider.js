996f5ce3c5c5d95283480b05cf3335a7544b45c6
marsolan <marvin.solano@groupeconnect.com>
2018-04-14 12:37:50 -0600

    Masthead slider ready and responsive design

---
+++ app/components/mastheadslider/mastheadslider.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.mastheadslider = (function () {
+
+    var bindEventsToUI = function () {
+        $('.mastheadslider .mastheadslider__slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true/*,
+            autoplay: true,
+            autoplaySpeed: 5000*/
+        });
+        $('.mastheadslider__controls--left').click(function(){
+            $('.mastheadslider ul').slick('slickPrev');
+        });
+        $('.mastheadslider__controls--right').click(function(){
+            $('.mastheadslider ul').slick('slickNext');
+        });
+    };
+
+    var init = function () {
+        console.log('APP.mastheadslider');
+        bindEventsToUI();
+    };
+
+    /**
+     * interfaces to public functions
+     */
+    return {
+        init: init
+    };
+
+}());
